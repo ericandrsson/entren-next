ff2f43d25aaf029d06d7f3cf823e696272f9871c
Eric Andersson <eric.andersson@ilpvfx.com>
2024-09-15 18:08:55 +0200

    automatic

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import '../styles/spots.css';
-// ... other imports
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
