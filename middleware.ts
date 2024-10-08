2432091916eea44f2128b8672142764ff5fd1735
Eric <eric.andrsson@gmail.com>
2024-10-08 23:04:16 +0200

    automatic

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,24 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // If there's no session and the user is trying to access a protected route,
+  // redirect them to the login page
+  if (!session && req.nextUrl.pathname.startsWith('/private')) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  return res
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,25 @@ import Link from "next/link";
 import { Button } from "@/src/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
 
-export default function Header() {
+export default async function Header() {
   const supabase = createClient();
-  const { data, error } = supabase.auth.getUser()
-  console.log(data, error);
+  const { data: { user } } = await supabase.auth.getUser();
+
   return (
     <header className="w-full bg-background shadow-sm">
       <div className="mx-auto px-4 py-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-primary">
           Entren
         </Link>
-        <Link href="/login">
-          <Button variant="default" className="text-white">
-            Logga in
-          </Button>
-        </Link>
+        {user ? (
+          <span>{user.email}</span>
+        ) : (
+          <Link href="/login">
+            <Button variant="default" className="text-white">
+              Logga in
+            </Button>
+          </Link>
+        )}
       </div>
     </header>
   );
