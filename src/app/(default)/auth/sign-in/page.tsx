0211b0c913b870f68b312d985dee000585b36ba5
Eric Andersson <eric.andersson@ilpvfx.com>
2024-10-20 16:31:06 +0200

    automatic

--- src/app/(default)/auth/sign-in/page.tsx
+++ src/app/(default)/auth/sign-in/page.tsx
@@ -1,10 +1,38 @@
-import { Suspense } from "react";
-import SignInPageContent from "@/src/components/auth/SignInPageContent";
+"use client";
+
+import { RequestResetPasswordForm } from "@/src/components/auth/RequestResetPasswordForm";
+import SignInForm from "@/src/components/auth/SignInForm";
+import { useToast } from "@/src/hooks/use-toast";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function SignInPage() {
+  const { toast } = useToast();
+  const searchParams = useSearchParams();
+  const [showResetPassword, setShowResetPassword] = useState(false);
+
+  useEffect(() => {
+    const storedToast = localStorage.getItem("authToast");
+    if (storedToast) {
+      const toastData = JSON.parse(storedToast);
+      toast(toastData);
+      localStorage.removeItem("authToast");
+    }
+
+    if (searchParams.get("reset") === "true") {
+      setShowResetPassword(true);
+    }
+  }, [toast, searchParams]);
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SignInPageContent />
-    </Suspense>
+    <>
+      {showResetPassword ? (
+        <RequestResetPasswordForm
+          onCancel={() => setShowResetPassword(false)}
+        />
+      ) : (
+        <SignInForm onResetPassword={() => setShowResetPassword(true)} />
+      )}
+    </>
   );
 }
